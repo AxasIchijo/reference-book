d1aa48d20e16c561d76f569220842bfae70bcf1b
Ichijo Hiroki <hiroki.ichijo@axas-japan.co.jp>
2022-03-02 19:37:41 +0900

    一旦コミット

--- reference-book/ViewController.swift
+++ reference-book/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource{
+    
+    var todoList = [String]()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 長さを返却
+        return self.todoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルを取得する
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
+        
+        let todoTitle = self.todoList[indexPath.row]
+        cell.textLabel?.text = todoTitle
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapAddButton(_ sender: Any) {
+        // ダイアログ生成
+        let alertController = UIAlertController(title: "Todo追加", message: "Todoを入力してください。", preferredStyle: UIAlertController.Style.alert)
+        alertController.addTextField(configurationHandler: nil)
+        
+        // OKボタン追加
+        let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) {
+            (action: UIAlertAction) in
+            if let textField = alertController.textFields?.first {
+                // 入力値をTodoリストに挿入
+                self.todoList.insert(textField.text!,at: 0)
+                self.tableView.insertRows(at: [IndexPath(row:0,section: 0)], with: UITableView.RowAnimation.right)
+            }
+        }
+        alertController.addAction(okAction)
+        
+        // キャンセルボタン追加
+        let cancelButton = UIAlertAction(title: "CANCEL", style: UIAlertAction.Style.cancel, handler: nil)
+        alertController.addAction(cancelButton)
+        
+        // ダイアログ表示
+        present(alertController, animated: true, completion: nil)
+    }
+    
 }
 
